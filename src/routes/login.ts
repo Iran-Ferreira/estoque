37175bc81365435615e55648eeec16715daa0d88
nari-escola <nariferreira712@gmail.com>
2025-05-29 22:02:17 -0300

    organizing the project

--- src/routes/login.ts
+++
@@ -1,9 +0,0 @@
-import authController  from "../controllers/authController.ts"
- 
-import { Router, Request, Response } from "express"
-
-const routes = Router()
-routes.post("/login", authController.logar);
-routes.post("/loginADM", authController.logar);
-
-export default routes;
